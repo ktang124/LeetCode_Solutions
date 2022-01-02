8da776c3b2fcaa96e1c2b30811a784aaa8710175
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-01-02 11:35:43 -0500

    Time: 10 ms (11.18%), Space: 41.6 MB (5.58%) - LeetHub

--- triangle/triangle.java
+++ triangle/triangle.java
@@ -1,31 +1,14 @@
 class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
-        int[][] dp = new int[triangle.size()][triangle.size()];
-        /*initialize dp triangle, more like a square*/
-        //fill everything with max
-        for(int[] d : dp){
-            Arrays.fill(d, Integer.MAX_VALUE-5);
-        }
-        
-        
-        dp[0][0] = triangle.get(0).get(0);
-        //base case is that the min path from the top always contains the top element
-        
-        for(int row = 1; row < dp.length; row++){
-            for(int col = 0; col <= row; col++){
-                int min = Integer.MAX_VALUE;// try to get the lowest tabulated value;
-                if(col != 0){
-                    min = Math.min(dp[row-1][col-1],min);
-                }
-                min = Math.min(dp[row-1][col],min);
-                dp[row][col] = triangle.get(row).get(col) + min;
+        List<Integer> belowRow = triangle.get(triangle.size() - 1);
+        for (int row = triangle.size() - 2; row >= 0; row--) {
+            List<Integer> currRow = new ArrayList<>();
+            for (int col = 0; col <= row; col++) {
+                int bestBelow = Math.min(belowRow.get(col), belowRow.get(col + 1));
+                currRow.add(bestBelow + triangle.get(row).get(col));
             }
+            belowRow = currRow;
         }
-        int res = Integer.MAX_VALUE;
-        for(int i = 0; i < triangle.size();i++){
-            res = Math.min(dp[triangle.size()-1][i], res);
-        }
-        return res;
+        return belowRow.get(0);
     }
-    
 }
