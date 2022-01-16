b3e81bff9dec4f15aa3307d3e658d01fdc581113
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-01-16 00:23:17 -0500

    Time: 1 ms (99.70%), Space: 39.8 MB (59.10%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,55 +1,27 @@
-class Solution {
+public class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        
-        List<List<Integer>> result = new ArrayList<>();
-        
-        backtrack(result, nums, 0);
-        
-        return result;
-    }
-    
-    public void backtrack(List<List<Integer>> result, int[] nums, int start){
-        if(start == nums.length){
-            result.add(toList(nums));  //also convert array to list
-        }
-        else{
-            for(int i = start; i < nums.length; i++){
-                
-                //to check for duplicates
-                if(i != start && !canPermutate(start, i, nums)) continue;
-                //this method will check if number start has appeared before in list of nums
-                
-                swap(i, start, nums);
-                backtrack(result, nums, start + 1);
-                swap(i, start, nums);   //to bring it to initial position
-            }
-        }
-    }
-    
-    //to convert nums array to list
-    public List<Integer> toList(int[] nums){
-        List<Integer> res = new ArrayList<>();
-        for(int i : nums)
-            res.add(i);
-        
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums==null || nums.length==0) return res;
+        boolean[] used = new boolean[nums.length];
+        List<Integer> list = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        dfs(nums, used, list, res);
         return res;
     }
-    
-    public void swap(int i, int j, int[] nums){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    }
-    
-    public boolean canPermutate(int start, int curr, int[] nums){
-        for(int i = start; i < curr; i++){
-            if(nums[i] == nums[curr]){
-                return false;
-            }
+
+    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
+        if(list.size()==nums.length){
+            res.add(new ArrayList<Integer>(list));
+            return;
         }
-        return true;
+        for(int i=0;i<nums.length;i++){
+            if(used[i]) continue;
+            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;
+            used[i]=true;
+            list.add(nums[i]);
+            dfs(nums,used,list,res);
+            used[i]=false;
+            list.remove(list.size()-1);
+        }
     }
 }
-
-//SC = O(1)
-//TC = O(N!)
