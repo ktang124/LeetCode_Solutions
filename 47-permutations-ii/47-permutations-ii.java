77e66e57343c6de8b877d70e3278667be2bb1bfc
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-01-16 00:18:46 -0500

    Time: 0 ms (100.00%), Space: 40.1 MB (42.88%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.java
+++ 47-permutations-ii/47-permutations-ii.java
@@ -1,25 +1,55 @@
 class Solution {
     public List<List<Integer>> permuteUnique(int[] nums) {
-        Set<List<Integer>> perms = new HashSet<>();
-        Arrays.sort(nums);
-        if(nums == null) return new ArrayList<>();
-        boolean[] visited = new boolean[nums.length];
-        dfs(perms, visited, nums, new ArrayList<>());
-        return new ArrayList<>(perms);
+        
+        List<List<Integer>> result = new ArrayList<>();
+        
+        backtrack(result, nums, 0);
+        
+        return result;
     }
-    private void dfs(Set<List<Integer>> perms, boolean[] visited, int[] nums, List<Integer> perm){
-        if(perm.size() == nums.length){
-            perms.add(new ArrayList<>(perm));
-            return;
+    
+    public void backtrack(List<List<Integer>> result, int[] nums, int start){
+        if(start == nums.length){
+            result.add(toList(nums));  //also convert array to list
         }
-        for(int i = 0; i < nums.length; i++){
-            if(!visited[i]){
-                visited[i] = true;
-                perm.add(nums[i]);
-                dfs(perms, visited, nums, perm);
-                visited[i] = false;
-                perm.remove(perm.size()-1);
+        else{
+            for(int i = start; i < nums.length; i++){
+                
+                //to check for duplicates
+                if(i != start && !canPermutate(start, i, nums)) continue;
+                //this method will check if number start has appeared before in list of nums
+                
+                swap(i, start, nums);
+                backtrack(result, nums, start + 1);
+                swap(i, start, nums);   //to bring it to initial position
             }
         }
     }
+    
+    //to convert nums array to list
+    public List<Integer> toList(int[] nums){
+        List<Integer> res = new ArrayList<>();
+        for(int i : nums)
+            res.add(i);
+        
+        return res;
+    }
+    
+    public void swap(int i, int j, int[] nums){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    
+    public boolean canPermutate(int start, int curr, int[] nums){
+        for(int i = start; i < curr; i++){
+            if(nums[i] == nums[curr]){
+                return false;
+            }
+        }
+        return true;
+    }
 }
+
+//SC = O(1)
+//TC = O(N!)
