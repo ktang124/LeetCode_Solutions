6eed4bb38a36017b355f5ec7fa212412d503ad14
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-01-17 00:10:44 -0500

    Time: 4 ms (99.79%), Space: 40.1 MB (11.44%) - LeetHub

--- 79-word-search/79-word-search.java
+++ 79-word-search/79-word-search.java
@@ -1,28 +1,86 @@
 class Solution {
+   private static final int[][] DIRS = new int[][] {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+    
     public boolean exist(char[][] board, String word) {
-        for(int row = 0; row < board.length; row++){
-            for(int col = 0; col < board[0].length; col++){
-                if(board[row][col] == word.charAt(0)){
-                    boolean[][] visited = new boolean[board.length][board[0].length];
-                    if(search(board, row, col, word, visited, 0) || word.length() == 1) return true;
-                }
+        if (board.length == 0 || board[0].length == 0 || word.length() == 0)
+            return false;
+        
+        if (!charsExist(board, word) || !charNeighborsExist(board, word)) return false;
+        
+        char firstChar = word.charAt(0);
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] == firstChar &&
+                   wordExists(board, word, 0, i, j))
+                    return true;
             }
         }
+        
         return false;
     }
     
-    private boolean search(char[][] board, int row, int col, String word, boolean[][] visited, int index){
-        if(index == word.length()) return true;
-        if(visited[row][col]) return false;
-        if(board[row][col] != word.charAt(index)) return false;
+    private boolean charsExist(char[][] board, String word) {
+        int[] chars = new int[255];
         
-        visited[row][col] = true;
-        boolean left = (col > 0 && search(board,row,col-1,word,visited,index+1));
-        boolean top = (row > 0 && search(board,row-1, col, word,visited, index+1));
-         boolean right = (col < board[0].length-1 && search(board,row, col+1, word,visited, index+1));
-         boolean bot = (row < board.length-1 && search(board,row+1, col, word,visited, index+1));
-        visited[row][col] = false;
-        return left || top || right || bot;
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                chars[board[i][j]]++;
+            }
+        }
         
+        for (char c : word.toCharArray()) {
+            if (--chars[c] < 0) return false;
+        }
+        
+        return true;
+    }
+    
+    private boolean charNeighborsExist(char[][] board, String word) {
+        boolean[][] neighborMap = new boolean[128][128];
+        
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (i > 0) {
+                    //setNeighbor(neighborMap, board[i - 1][j], board[i][j]);
+                    char c1 = board[i - 1][j], c2 = board[i][j];
+                    neighborMap[c1][c2] = true;
+                    neighborMap[c2][c1] = true;
+                }
+                if (j > 0) {
+                    //setNeighbor(neighborMap, board[i][j - 1], board[i][j]);
+                    char c1 = board[i][j - 1], c2 = board[i][j];
+                    neighborMap[c1][c2] = true;
+                    neighborMap[c2][c1] = true;
+                }
+            }
+        }
+        
+        for (int i = 1; i < word.length(); i++) {
+            if (!neighborMap[word.charAt(i - 1)][word.charAt(i)])
+                return false;
+        }
+        
+        return true;
+    }
+    
+    private void setNeighbor(boolean[][] neighborMap, char c1, char c2) {
+        neighborMap[c1][c2] = true;
+        neighborMap[c2][c1] = true;
+    }
+    
+    private boolean wordExists(char[][] board, String word, int index, int x, int y) {
+        if (index == word.length()) return true;
+        if (x < 0 || x >= board.length || y < 0 || y >= board[0].length) return false;
+        if (board[x][y] != word.charAt(index)) return false;
+        
+        board[x][y] -= 26;
+        for (int[] dir : DIRS) {
+            if (wordExists(board, word, index + 1, x + dir[0], y + dir[1])) {
+                return true;
+            }
+        }
+        board[x][y] += 26;
+        
+        return false;
     }
 }
