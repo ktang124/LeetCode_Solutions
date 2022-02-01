b7a74d91db967397b9c7c1387867a4dedc533b19
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-02-01 00:04:03 -0500

    Time: 94 ms (99.42%), Space: 214 MB (28.65%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.rb
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.rb
@@ -1,15 +1,9 @@
 # @param {String} s
 # @return {Integer}
 def first_uniq_char(s)
-    charCount = Hash.new(0)
-    for i in 0..s.length-1 do
-        curCount = charCount[s[i]]
-        charCount[s[i]] = curCount + 1
-    end
-    for i in 0..s.length-1 do
-        if charCount[s[i]] == 1 then
-            return i;
-        end
-    end
-    -1
+  result = []
+  ("a".."z").each do |letter|
+    result << s.index(letter) if s.count(letter) == 1
+  end
+  result.empty? ? -1 : result.min
 end
