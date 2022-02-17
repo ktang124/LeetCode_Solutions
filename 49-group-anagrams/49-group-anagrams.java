eb559db67106e36807e0cda7d046ffb749599bee
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-02-16 23:45:20 -0500

    Time: 10 ms (74.18%), Space: 55.3 MB (32.13%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.java
+++ 49-group-anagrams/49-group-anagrams.java
@@ -1,23 +1,16 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         if(strs==null || strs.length==0) return new ArrayList<>();
-        Map<String,List<String>> map = new HashMap<>();
-        // for each string: 1. using char[] store s.toCharArray(),  sort the char array, convert it to string (String.valueOf(ca)), use if as key
-        // 2. arr = new char[26]
-        for(String s:strs){
-            // char[] arr = new char[26];
-            // for(char c:s.toCharArray()){
-            //     arr[c-'a']++;
-            // }
-             //String key = String.valueOf(arr);
-            
-            char[] charArr = s.toCharArray();
-            Arrays.sort(charArr);
-            String key = String.valueOf(charArr);
-          
-            if(!map.containsKey(key)) map.put(key,new ArrayList<>());
-            map.get(key).add(s);
+        Map<String, List<String>> hm = new HashMap<>();
+        
+        for(String str : strs){
+            char[] sArray = str.toCharArray();
+            Arrays.sort(sArray);
+            String key = new String(sArray);
+            List<String> curList = hm.getOrDefault(key, new ArrayList<>());
+            curList.add(str);
+            hm.put(key, curList);
         }
-        return new ArrayList(map.values());
+        return new ArrayList<>(hm.values());
     }
 }
