c18b3d223d4475d5d7d385abd3c48521b1981a4d
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-02-23 21:20:58 -0500

    Time: 4 ms (94.21%), Space: 44 MB (80.33%) - LeetHub

--- 155-min-stack/155-min-stack.java
+++ 155-min-stack/155-min-stack.java
@@ -1,35 +1,35 @@
 class MinStack {
-    Deque<Integer> stack;
-    PriorityQueue<Integer> minStack;
-    public MinStack() {
-        stack = new ArrayDeque<>();
-        minStack = new PriorityQueue<>();
+    
+    private Stack<int[]> stack = new Stack<>();
+    
+    public MinStack() { }
+    
+    
+    public void push(int x) {
+        
+        /* If the stack is empty, then the min value
+         * must just be the first value we add. */
+        if (stack.isEmpty()) {
+            stack.push(new int[]{x, x});
+            return;
+        }
+        
+        int currentMin = stack.peek()[1];
+        stack.push(new int[]{x, Math.min(x, currentMin)});
     }
     
-    public void push(int val) {
-        stack.push(val);
-        minStack.add(val);
-    }
     
     public void pop() {
-        int remove = stack.pop();
-        minStack.remove(remove);
+        stack.pop();
     }
+    
     
     public int top() {
-        return stack.peek();        
+        return stack.peek()[0];
     }
     
+    
     public int getMin() {
-        return minStack.peek();
+        return stack.peek()[1];
     }
 }
-
-/**
- * Your MinStack object will be instantiated and called as such:
- * MinStack obj = new MinStack();
- * obj.push(val);
- * obj.pop();
- * int param_3 = obj.top();
- * int param_4 = obj.getMin();
- */
