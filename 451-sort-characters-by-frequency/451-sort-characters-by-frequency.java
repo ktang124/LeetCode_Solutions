d4d9f06fb66f1952ccae8220aae40d9607f581c7
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-03-01 20:42:30 -0500

    Time: 10 ms (90.31%), Space: 46 MB (63.24%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,5 +1,7 @@
 class Solution {
     public String frequencySort(String s) {
+        
+        //we make a map of the char ascii value to the number of that char present
         int[][] map = new int[128][2];
         for(int i = 0; i < 128; i++){
             map[i][0] = i;
@@ -7,7 +9,13 @@ class Solution {
         for(int i = 0; i < s.length(); i++){
             map[s.charAt(i)][1] += 1;
         }
+        
+        //we can now sort the array by the descending order of the number of each char
         Arrays.sort(map, (o1, o2) -> (o2[1] - o1[1]));
+        
+        //we can take advantage of the fact that the result string will only be s.length() so 
+        //it is possible to get rid of the overhead of stringbuffer and only use char array
+        //to build the result string
         char[] res = new char[s.length()];
         int index = 0;
         for(int i = 0; i < 128; i++){
