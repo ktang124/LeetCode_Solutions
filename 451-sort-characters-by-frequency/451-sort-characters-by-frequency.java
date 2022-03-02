0388ba04aaf95c0b4a3a2ef9dcfeac00b6bda60a
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-03-01 20:44:41 -0500

    Time: 39 ms (25.26%), Space: 54.9 MB (14.62%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,30 +1,35 @@
 class Solution {
     public String frequencySort(String s) {
         
-        //we make a map of the char ascii value to the number of that char present
-        int[][] map = new int[128][2];
-        for(int i = 0; i < 128; i++){
-            map[i][0] = i;
-        }
-        for(int i = 0; i < s.length(); i++){
-            map[s.charAt(i)][1] += 1;
-        }
-        
-        //we can now sort the array by the descending order of the number of each char
-        Arrays.sort(map, (o1, o2) -> (o2[1] - o1[1]));
-        
-        //we can take advantage of the fact that the result string will only be s.length() so 
-        //it is possible to get rid of the overhead of stringbuffer and only use char array
-        //to build the result string
-        StringBuffer res = new StringBuffer();
-        int index = 0;
-        for(int i = 0; i < 128; i++){
-            int cur = map[i][1];
-            for(int j = 0; j < cur; j++){
-               res.append((char)(map[i][0]));
+    if (s == null || s.isEmpty()) return s;
+    
+    // Count up the occurances.
+    Map<Character, Integer> counts = new HashMap<>();
+    for (char c : s.toCharArray()) {
+        counts.put(c, counts.getOrDefault(c, 0) + 1);
+    }
+    
+    int maximumFrequency = Collections.max(counts.values());
+    
+    // Make the list of buckets and apply bucket sort.
+    List<List<Character>> buckets = new ArrayList<>();
+    for (int i = 0; i <= maximumFrequency; i++) {
+        buckets.add(new ArrayList<Character>());
+    }
+    for (Character key : counts.keySet()) {
+        int freq = counts.get(key);
+        buckets.get(freq).add(key);
+    }
+
+    // Build up the string. 
+    StringBuilder sb = new StringBuilder();
+    for (int i = buckets.size() - 1; i >= 1; i--) {
+        for (Character c : buckets.get(i)) {
+            for (int j = 0; j < i; j++) {
+                sb.append(c);
             }
         }
-        return new String(res);
-        
     }
+    return sb.toString();
+}
 }
