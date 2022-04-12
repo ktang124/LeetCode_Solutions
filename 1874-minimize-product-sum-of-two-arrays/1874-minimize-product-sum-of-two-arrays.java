0b9fefb570a1f625b5aa10dbab61bd3514e32c1d
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-04-11 23:55:05 -0400

    Time: 8 ms (83.44%), Space: 93.4 MB (51.42%) - LeetHub

--- 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
+++ 1874-minimize-product-sum-of-two-arrays/1874-minimize-product-sum-of-two-arrays.java
@@ -2,13 +2,25 @@ class Solution {
     public int minProductSum(int[] nums1, int[] nums2) {
         //[1,2,4,5,7]
         //[8,6,4,3,2]
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
+        sort(nums1);
+        sort(nums2);
         int prod = 0;
         int n = nums1.length;
         for(int i = 0; i < n; i++){
             prod += nums1[i]*nums2[n-i-1];
         }
         return prod;
+    }
+    private void sort(int[] nums){
+        int[] map = new int[100];
+        for(int i = 0; i < nums.length; i++){
+            map[nums[i]-1] += 1;
+        }
+        int index = 0;
+        for(int i = 0; i < 100; i++){
+            for(int j = 0; j < map[i]; j++){
+                nums[index++] = i+1;
+            }
+        }
     }
 }
