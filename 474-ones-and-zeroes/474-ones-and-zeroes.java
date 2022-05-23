2ca0c845666cb4bea05643296bc99099721987ea
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-05-23 12:49:56 -0400

    Time: 36 ms (73.30%), Space: 42.2 MB (83.94%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.java
+++ 474-ones-and-zeroes/474-ones-and-zeroes.java
@@ -1,27 +1,25 @@
-public class Solution {
+class Solution {
     public int findMaxForm(String[] strs, int m, int n) {
-        int[][][] memo = new int[strs.length][m + 1][n + 1];
-        return calculate(strs, 0, m, n, memo);
-    }
-    public int calculate(String[] strs, int i, int zeroes, int ones, int[][][] memo) {
-        if (i == strs.length)
-            return 0;
-        if (memo[i][zeroes][ones] != 0)
-            return memo[i][zeroes][ones];
-        int[] count = countzeroesones(strs[i]);
-        int taken = -1;
-        if (zeroes - count[0] >= 0 && ones - count[1] >= 0)
-            taken = calculate(strs, i + 1, zeroes - count[0], ones - count[1], memo) + 1;
-        int not_taken = calculate(strs, i + 1, zeroes, ones, memo);
-        memo[i][zeroes][ones] = Math.max(taken, not_taken);
-        return memo[i][zeroes][ones];
-    }
-    public int[] countzeroesones(String s) {
-        int[] c = new int[2];
-        for (int i = 0; i < s.length(); i++) {
-            c[s.charAt(i)-'0']++;
+        int[][] dp = new int[m + 1][n + 1];
+        
+        for (String str : strs) {
+            int countZero = 0, countOne = 0;
+            
+            for (char c : str.toCharArray()) {
+                if (c == '0') {
+                    countZero++;
+                } else {
+                    countOne++;
+                }
+            }
+            
+            for (int i = m; i >= countZero; i--) {
+                for (int j = n; j >= countOne; j--) {
+                    dp[i][j] = Math.max(dp[i][j], dp[i - countZero][j - countOne] + 1);
+                }
+            }
         }
-        return c;
+        
+        return dp[m][n];
     }
 }
-
