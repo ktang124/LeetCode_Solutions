c2d46ac4e01eaf3c4266f95fd2f6b6c21ee49d57
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-05-23 11:47:54 -0400

    Time: 36 ms (17.78%), Space: 54.2 MB (39.89%) - LeetHub

--- 277-find-the-celebrity/277-find-the-celebrity.java
+++ 277-find-the-celebrity/277-find-the-celebrity.java
@@ -1,22 +1,28 @@
-/* The knows API is defined in the parent class Relation.
-      boolean knows(int a, int b); */
-
 public class Solution extends Relation {
+    
+    private int numberOfPeople;
+    
     public int findCelebrity(int n) {
-       int[] degree = new int[n];
-        int[] numEdges = new int[n];
-        //false default, if true, the index number could be celebrity
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < n; j++){
-                if(i != j  && knows(i,j)){
-                    degree[j] += 1;
-                    numEdges[i] += 1;
-                }
+        numberOfPeople = n;
+        int celebrityCandidate = 0;
+        for (int i = 0; i < n; i++) {
+            if (knows(celebrityCandidate, i)) {
+                celebrityCandidate = i;
             }
         }
-        for(int i = 0; i < n; i++){
-            if((degree[i] == n-1) && numEdges[i] == 0) return i;
+        if (isCelebrity(celebrityCandidate)) {
+            return celebrityCandidate;
         }
         return -1;
+    }
+    
+    private boolean isCelebrity(int i) {
+        for (int j = 0; j < numberOfPeople; j++) {
+            if (i == j) continue; // Don't ask if they know themselves.
+            if (knows(i, j) || !knows(j, i)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
