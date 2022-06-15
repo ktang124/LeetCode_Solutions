de5aa856910aa37725b3e6a520fe74c4c00645e2
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-06-14 22:07:00 -0400

    Time: 268 ms (35.23%), Space: 23.7 MB (45.14%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -1,7 +1,29 @@
 class Solution:
     def singleNonDuplicate(self, nums: List[int]) -> int:
-        single = 0
-        for num in nums:
-            single ^= num
-            
-        return single
+        left = 0
+        right = len(nums)-1
+        #len of nums is always odd because of 2k+1 elements
+        while left <= right:
+            mid = left + (right-left)//2
+            curNum = nums[mid]
+            isLeftSame = mid > 0 and nums[mid-1] == curNum
+            isRightSame = mid < len(nums) - 1 and nums[mid+1] == curNum
+            if not isLeftSame and not isRightSame:
+                return nums[mid]
+            elif mid % 2 == 0:
+                if isLeftSame:
+                    right = mid-1
+                else:
+                    left = mid+1
+            else:
+                if isLeftSame:
+                    left = mid+1
+                else:
+                    right = mid-1
+                    
+        return -1
+               
+                
+                
+        #[1,1,2,3,3,4,4,8,8] len 9, mid = 4
+        #[3,3,7,7,8,8,10,11,11] len 9, mid = 4
