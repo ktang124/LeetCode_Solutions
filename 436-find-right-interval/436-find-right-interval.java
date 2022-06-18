537d784aa742462a6ba8a3facc9583349eb0b9d8
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-06-18 15:16:50 -0400

    Time: 637 ms (10.13%), Space: 47.1 MB (87.36%) - LeetHub

---
+++ 436-find-right-interval/436-find-right-interval.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int[] findRightInterval(int[][] intervals) {
+        int[] res = new int[intervals.length];
+        for (int i = 0; i < intervals.length; i++) {
+            int min = Integer.MAX_VALUE;
+            int minindex = -1;
+            for (int j = 0; j < intervals.length; j++) {
+                if (intervals[j][0] >= intervals[i][1] && intervals[j][0] < min) {
+                    min = intervals[j][0];
+                    minindex = j;
+                }
+            }
+            res[i] = minindex;
+        }
+        return res;
+    }
+}
