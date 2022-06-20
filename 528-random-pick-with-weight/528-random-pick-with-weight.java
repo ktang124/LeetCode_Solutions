919a5d48d40d340ffcd6ca9238d819e41ae97cfd
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-06-19 22:17:45 -0400

    Time: 40 ms (50.88%), Space: 57 MB (37.45%) - LeetHub

--- 528-random-pick-with-weight/528-random-pick-with-weight.java
+++ 528-random-pick-with-weight/528-random-pick-with-weight.java
@@ -15,13 +15,17 @@ class Solution {
 
     public int pickIndex() {
         double target = this.totalSum * Math.random();
-        int i = 0;
-        // run a linear search to find the target zone
-        for (; i < this.prefixSums.length; ++i) {
-            if (target < this.prefixSums[i])
-                return i;
+
+        // run a binary search to find the target zone
+        int low = 0, high = this.prefixSums.length;
+        while (low < high) {
+            // better to avoid the overflow
+            int mid = low + (high - low) / 2;
+            if (target > this.prefixSums[mid])
+                low = mid + 1;
+            else
+                high = mid;
         }
-        // to have a return statement, though this should never happen.
-        return i - 1;
-  }
+        return low;
+    }
 }
