396d0d47549dc7041d260e1ea343a30477c8ffbc
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-06-28 20:48:06 -0400

    Time: 557 ms (8.20%), Space: 14.8 MB (93.95%) - LeetHub

--- 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
+++ 1647-minimum-deletions-to-make-character-frequencies-unique/1647-minimum-deletions-to-make-character-frequencies-unique.py
@@ -1,25 +1,28 @@
 class Solution:
     def minDeletions(self, s: str) -> int:
-        s = sorted(s)
-        deletes = 0
-        counts = set()
-        curCount = 1
-        for i in range(1,len(s)):
-            prev = s[i-1]
-            cur = s[i]
-            if cur != prev:
-                while curCount > 0 and curCount in counts:
-                    deletes += 1
-                    curCount -= 1
-                    
-                counts.add(curCount)
-                curCount = 1
-            else:
-                curCount += 1
-                
-        while curCount > 0 and curCount in counts:
-            deletes += 1
-            curCount -= 1
-        return deletes
-            
         
+        # Store the frequency of each character
+        frequency = [0] * 26
+        for char in s:
+            frequency[ord(char) - ord('a')] += 1
+            
+        # Add all non-zero frequencies to max priority queue
+        # Create a max priority queue by flipping the sign of each element
+        pq = [-freq for freq in frequency if freq != 0]
+        heapq.heapify(pq)
+        
+        delete_count = 0
+        while len(pq) > 1:
+            # Flip the sign back to positive when removing from the max priority queue
+            top_element = -heapq.heappop(pq)
+            
+            # If the top two elements in the priority queue are the same
+            if top_element == -pq[0]:
+                # Decrement the popped value and push it back into the queue
+                if top_element - 1 > 0: 
+                    top_element -= 1
+                    heapq.heappush(pq, -top_element)
+
+                delete_count += 1
+        
+        return delete_count
