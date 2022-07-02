8d5ea917e5ed2452c4cf6b375cf5bd2f699f8498
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-07-02 15:28:57 -0400

    Time: 121 ms (78.59%), Space: 19.3 MB (28.12%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,7 +1,10 @@
 class Solution:
     def topKFrequent(self, nums, k):
+        #make buckets of each frequency, an element can appear at most k times
         bucket = [[] for _ in range(len(nums) + 1)]
-        Count = Counter(nums).items()  
-        for num, freq in Count: bucket[freq].append(num) 
+        #make a counter of the items
+        Count = Counter(nums)  
+        for num, freq in Count.items():
+            bucket[freq].append(num) 
         flat_list = [item for sublist in bucket for item in sublist]
-        return flat_list[::-1][:k]
+        return flat_list[len(flat_list)-k:]
