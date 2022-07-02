8da67f32161ea60936d0d238a53520b570885ce6
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-07-02 15:26:28 -0400

    Time: 183 ms (31.12%), Space: 19.3 MB (28.12%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,26 +1,7 @@
-class Solution {
-    public int[] topKFrequent(int[] nums, int k) {
-        Map<Integer, Integer> hm = new HashMap<>();
-        for(int num : nums){
-            int count = hm.getOrDefault(num, 0) + 1;
-            hm.put(num, count);
-        }
-        
-        int[] res = new int[k];
-        int index = 0;
-        for(int i = 0; i < k; i++){
-            int max = -1;
-            int maxKey = 0;
-            for(int key : hm.keySet()){
-                if(hm.get(key) > max){
-                    max = hm.get(key);
-                    maxKey = key;
-                }
-            }
-            res[index] = maxKey;
-            hm.put(maxKey, -2);
-            index++;
-        }
-        return res;
-    }
-}
+class Solution:
+    def topKFrequent(self, nums, k):
+        bucket = [[] for _ in range(len(nums) + 1)]
+        Count = Counter(nums).items()  
+        for num, freq in Count: bucket[freq].append(num) 
+        flat_list = [item for sublist in bucket for item in sublist]
+        return flat_list[::-1][:k]
