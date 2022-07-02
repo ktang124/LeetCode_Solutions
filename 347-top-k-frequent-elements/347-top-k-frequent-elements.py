72e1538ac72e1df5ff96fd0013b0479c98562ae2
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-07-02 15:16:30 -0400

    Time: 183 ms (31.12%), Space: 19.3 MB (28.12%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.py
@@ -1,6 +1,7 @@
 class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        counts = Counter(nums)
-        #sort all the keys by greatest to least by count
-        sortList = sorted(counts.keys(), key = lambda x: counts[x], reverse = True)
-        return sortList[:k]
+    def topKFrequent(self, nums, k):
+        bucket = [[] for _ in range(len(nums) + 1)]
+        Count = Counter(nums).items()  
+        for num, freq in Count: bucket[freq].append(num) 
+        flat_list = [item for sublist in bucket for item in sublist]
+        return flat_list[::-1][:k]
