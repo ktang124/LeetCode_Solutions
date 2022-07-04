27508dacc2c16454d16f613adb93b00c90e4b7c2
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-07-04 00:32:32 -0400

    Time: 125 ms (13.55%), Space: 14.4 MB (16.89%) - LeetHub

--- 256-paint-house/256-paint-house.py
+++ 256-paint-house/256-paint-house.py
@@ -1,38 +1,25 @@
 class Solution:
-    def minCost(self, costs: List[List[int]]) -> int:
-        #at each step, you are making a decision on which color to choose, this means there are 2^n total outcomes
-        cache = [[0] * 3 for i in range(len(costs))]
-        ans1 = self.dfs(costs, len(costs)-1, 0, cache)
-        ans2 = self.dfs(costs, len(costs)-1, 1, cache)
-        ans3 = self.dfs(costs, len(costs)-1, 2, cache)
-        return min(ans1,ans2,ans3)
-    
-    def dfs(self, costs, index, prev, cache):
-        if(index == -1):
+    def minCost(self, costs):
+   
+
+        def paint_cost(n, color):
+            if (n, color) in self.memo:
+                return self.memo[(n, color)]
+            total_cost = costs[n][color]
+            if n == len(costs) - 1:
+                pass
+            elif color == 0:
+                total_cost += min(paint_cost(n + 1, 1), paint_cost(n + 1, 2))
+            elif color == 1:
+                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 2))
+            else:
+                total_cost += min(paint_cost(n + 1, 0), paint_cost(n + 1, 1))
+            self.memo[(n, color)] = total_cost
+            return total_cost
+
+        if costs == []:
             return 0
-        
-        if (cache[index][prev] != 0):
-            return cache[index][prev]
-        
-        if prev == 0: #cannot use red
-            useBlue = costs[index][1] + self.dfs(costs,index-1,1,cache)
-            useGreen = costs[index][2] + self.dfs(costs,index-1,2,cache)
-            
-            minCost = min(useBlue,useGreen)
-            cache[index][prev] = minCost
-            return minCost
-        elif prev == 1: #cannot use blue
-            useRed = costs[index][0] + self.dfs(costs,index-1,0,cache)
-            useGreen = costs[index][2] + self.dfs(costs,index-1,2,cache)
-            
-            minCost = min(useRed, useGreen)
-            cache[index][prev] = minCost
-            return minCost
-        elif prev == 2: #cannot use green
-            useRed = costs[index][0] + self.dfs(costs,index-1,0,cache)
-            useBlue = costs[index][1] + self.dfs(costs,index-1,1,cache)
-            
-            minCost = min(useBlue,useRed)
-            cache[index][prev] = minCost
-            return minCost
+
+        self.memo = {}
+        return min(paint_cost(0, 0), paint_cost(0, 1), paint_cost(0, 2))
        
