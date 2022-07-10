cd420961bc1a487c6ad22d5938a309b7c93f3cdb
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-07-10 01:13:10 -0400

    Time: 260 ms (29.28%), Space: 14.7 MB (62.53%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.py
@@ -4,8 +4,8 @@ class Solution:
         dp = [[0] * n for i in range(n+1)]
         for row in range(1, len(dp)):
             for col in range(n):
-                topLeft = dp[row-1][max(col-1,0)]
-                topRight = dp[row-1][min(col+1, n-1)]
+                topLeft = dp[row-1][max(col-1,0)] #max(col-1, 0) ensures that if col does not go out of bounds into index -1
+                topRight = dp[row-1][min(col+1, n-1)] #min(col+1, n-1) ensures that col does not go out of bounds into index n
                 dp[row][col] = min(topLeft,dp[row-1][col],topRight) + matrix[row-1][col]
                 
         return min(dp[-1])
