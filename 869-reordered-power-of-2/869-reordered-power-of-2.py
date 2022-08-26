596c5118873ae33e42ed3db3f775b695c5c63b07
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-08-25 23:20:13 -0400

    Time: 49 ms (72.19%), Space: 13.9 MB (26.74%) - LeetHub

---
+++ 869-reordered-power-of-2/869-reordered-power-of-2.py
@@ -0,0 +1,13 @@
+class Solution:
+    def reorderedPowerOf2(self, n: int) -> bool:
+        twoPows = [(2**i) for i in range(31)]
+        compare = sorted(str(n))
+        for twoPow in twoPows:
+            sortStr = sorted(str(twoPow))
+            if compare == sortStr:
+                return True
+            
+        return False
+        
+        
+        
