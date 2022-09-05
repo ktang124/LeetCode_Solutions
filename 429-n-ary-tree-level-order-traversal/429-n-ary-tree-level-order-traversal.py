d957eafac7372167aa756cc94e2a387ba0770d1d
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-09-05 01:33:21 -0400

    Time: 108 ms (12.13%), Space: 16.3 MB (10.93%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.py
@@ -1,27 +1,14 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val=None, children=None):
-        self.val = val
-        self.children = children
-"""
-
 class Solution:
-    def levelOrder(self, root: 'Node') -> List[List[int]]:
-        if root == None:
+    def levelOrder(self, root: Optional['Node']) -> List[List[int]]:
+        if root is None:
             return []
-        res = []
-        queue = [root]
+        result = []
+        queue = collections.deque([root])
         while queue:
-            size = len(queue)
-            levelLst = []
-            while size:
-                size -= 1
-                poll = queue.pop(0)
-                levelLst.append(poll.val)
-                for c in poll.children:
-                    queue.append(c)
-            res.append(levelLst)
-            
-        return res
-                
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                queue.extend(node.children)
+            result.append(level)
+        return result
