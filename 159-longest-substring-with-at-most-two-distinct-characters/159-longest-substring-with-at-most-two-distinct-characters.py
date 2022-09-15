55e7bc585be9ecf7e8656d5db82343515825a0a2
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-09-15 00:50:25 -0400

    Time: 784 ms (27.26%), Space: 14.7 MB (86.65%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
@@ -1,21 +1,31 @@
+from collections import defaultdict
 class Solution:
-    def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-        charWindow = defaultdict(int)
-        start = 0
-        maximum = 0
-        def verify(charWindow):
-            count = 0
-            for a in charWindow:
-                if a > 0:
-                    count += 1
-            return count <= 2
-        for end in range(len(s)):
-            charWindow[s[end]] += 1
-            while not verify(charWindow.values()):
-                charWindow[s[start]] -= 1
-                start += 1
-            maximum = max(maximum, end-start + 1)
-            
-        return maximum
-            
-                
+    def lengthOfLongestSubstringTwoDistinct(self, s: 'str') -> 'int':
+        n = len(s)
+        if n < 3:
+            return n
+
+        # sliding window left and right pointers
+        left, right = 0, 0
+        # hashmap character -> its rightmost position
+        # in the sliding window
+        hashmap = defaultdict()
+
+        max_len = 2
+
+        while right < n:
+            # when the slidewindow contains less than 3 characters
+            hashmap[s[right]] = right
+            right += 1
+
+            # slidewindow contains 3 characters
+            if len(hashmap) == 3:
+                # delete the leftmost character
+                del_idx = min(hashmap.values())
+                del hashmap[s[del_idx]]
+                # move left pointer of the slidewindow
+                left = del_idx + 1
+
+            max_len = max(max_len, right - left)
+
+        return max_len
