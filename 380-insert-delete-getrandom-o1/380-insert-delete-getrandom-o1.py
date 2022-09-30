3f8658de67574bbd7c7f6953977bac4139584e31
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-09-30 00:26:23 -0400

    Time: 971 ms (38.19%), Space: 61.5 MB (33.08%) - LeetHub

--- 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/380-insert-delete-getrandom-o1.py
@@ -1,29 +1,40 @@
-class RandomizedSet:
-
+from random import choice
+class RandomizedSet():
     def __init__(self):
-        self.randSet = set()
+        """
+        Initialize your data structure here.
+        """
+        self.dict = {}
+        self.list = []
 
+        
     def insert(self, val: int) -> bool:
-        if val in self.randSet:
+        """
+        Inserts a value to the set. Returns true if the set did not already contain the specified element.
+        """
+        if val in self.dict:
             return False
-        self.randSet.add(val)
+        self.dict[val] = len(self.list)
+        self.list.append(val)
         return True
+        
 
     def remove(self, val: int) -> bool:
-        if val in self.randSet:
-            self.randSet.remove(val)
+        """
+        Removes a value from the set. Returns true if the set contained the specified element.
+        """
+        if val in self.dict:
+            # move the last element to the place idx of the element to delete
+            last_element, idx = self.list[-1], self.dict[val]
+            self.list[idx], self.dict[last_element] = last_element, idx
+            # delete the last element
+            self.list.pop()
+            del self.dict[val]
             return True
-        else:
-            return False
+        return False
 
     def getRandom(self) -> int:
-        rand = randint(0,len(self.randSet)-1)
-        lst = list(self.randSet)
-        return lst[rand]
-
-
-# Your RandomizedSet object will be instantiated and called as such:
-# obj = RandomizedSet()
-# param_1 = obj.insert(val)
-# param_2 = obj.remove(val)
-# param_3 = obj.getRandom()
+        """
+        Get a random element from the set.
+        """
+        return choice(self.list)
