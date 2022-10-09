836cb2530100ec96eaf2b6988177b54c4d0aad2e
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-10-09 19:57:00 -0400

    Time: 115 ms (71.73%), Space: 17.5 MB (50.31%) - LeetHub

--- 253-meeting-rooms-ii/253-meeting-rooms-ii.py
+++ 253-meeting-rooms-ii/253-meeting-rooms-ii.py
@@ -1,23 +1,20 @@
 class Solution:
     def minMeetingRooms(self, intervals: List[List[int]]) -> int:
-        max_count = 0
-        starts = [interval[0] for interval in intervals]
-        ends = [interval[1] for interval in intervals]
-        starts.sort()
-        ends.sort()
-        sP = 0
-        eP = 0
-        count = 0
-        n = len(starts)
-        while sP < n:
-            if starts[sP] <ends[eP]:
-                #meeting started
-                count += 1
-                sP+=1
-                max_count = max(count,max_count)
-            else:
-                #meeting ended
-                eP+=1
-                count -= 1
+        if not intervals:
+            return 0
+        max_count = 1
+        #sort by start time
+        sort_intervals = sorted(intervals, key = lambda x: x[0])
+        pq = []
+        #add the first meeting
+        heapq.heappush(pq, sort_intervals[0][1])
+        for i in sort_intervals[1:]:
+            #If the room due to free up the earliest is free, assign that room to this meeting
+            if pq[0] <= i[0]:
+                heapq.heappop(pq)
+            #If a new room is to be assigned, we add it to the heap
+            #If an old room is allocated, then also we have to add to the heap with updated end time
+            heapq.heappush(pq, i[1])
+            max_count = max(len(pq),max_count)
                 
         return max_count
