3b6b15e744f2d41020d32e7163c0f23aa1f47e9b
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-11-09 22:47:01 -0500

    Time: 62 ms (25.83%), Space: 14 MB (13.56%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.py
@@ -1,16 +1,5 @@
 class Solution:
-   
     def numTrees(self, n: int) -> int:
-        #what is the recurrence relationship? 
-        #F(0) = 0
-        #F(K) = 1 + sum(F(0)-F(K-1))
-        dp = [0] * (n+1)
-        dp[0] = 1
-        dp[1] = 1
-        for i in range(1,n):
-            _sum = 0
-            for j in range(i+1):
-                _sum += dp[j] * dp[i-j]
-            dp[i+1] = _sum
-        print(dp)
+        dp = [1,1] + ([0] * (n-1))
+        for i in range(1,n): dp[i+1] = sum(dp[j] * dp[i-j] for j in range(i+1))
         return dp[n]
