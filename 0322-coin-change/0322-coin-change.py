72bf2e66b5ef840b01c4d707e4401e453849edd8
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-11-10 22:20:48 -0500

    Time: 2835 ms (64.74%), Space: 14.2 MB (60.76%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -3,5 +3,5 @@ class Solution:
         minCoins = [float('inf')] * (amount + 1)
         minCoins[0] = 0
         for i in range(1,amount+1):
-            minCoins[i] = min((1+minCoins[i-coin] if i-coin>=0 else float('inf')) for coin in coins)
+            minCoins[i] = 1+min((minCoins[i-coin] if i-coin>=0 else float('inf')) for coin in coins)
         return minCoins[amount] if minCoins[amount] != float('inf') else -1
