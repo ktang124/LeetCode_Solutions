9b599672a4482b4df545e52d11742a35b3a90f8c
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-11-11 21:53:54 -0500

    Time: 505 ms (99.04%), Space: 36.1 MB (57.44%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.py
@@ -1,21 +1,65 @@
-from sortedcontainers import SortedList
 class MedianFinder:
-
+    
     def __init__(self):
-        #naive solution
-        self.stream = SortedList()
+        self.small = []
+        self.large = [] 
 
-    def addNum(self, num: int) -> None:
-        self.stream.add(num)
-
-    def findMedian(self) -> float:
-        n = len(self.stream)
-        if n % 2 == 0:
-            median = self.stream[n//2] + self.stream[(n//2)-1]
-            return median/2
+    def addNum(self, num: int) -> None: # log(logn)
+        if len(self.small) == len(self.large):
+            heapq.heappush(self.large, -heappushpop(self.small, -num))
         else:
-            median = self.stream[n//2]
-            return median
+            heapq.heappush(self.small, -heappushpop(self.large, num))
+
+    def findMedian(self) -> float: # log(1)
+        if len(self.small) == len(self.large):
+            return float(self.large[0] - self.small[0]) / 2.0
+        else:
+            return self.large[0]
+
+
+'''
+    Idea
+        1. using two heap(min, max) for adding num => O(logn)
+            find median with heapMin[0] & heapMax[0] => O(1)
+    Implem
+        1. addNum
+            create small(max-hip) and large(min-heap)
+            add num to small if size is equal, and add largest num of samll to large
+            add num to large if size is not equal, and add smallest num of large to small
+            size small, large (k, k)
+            size small, large (k, k + 1)
+        2. findMedian
+            if size is equal = (small[0] + large[0]) / 2
+            if size is not equl = large[0]
+
+
+'''
+
+'''        
+    # sort array method
+    def __init__(self):
+        self.dataStream = []
+        self.isSorted = False
+        self.median = None
+
+    def addNum(self, num: int) -> None: # log(1)
+        if num is not None:
+            self.dataStream.append(num)
+            self.isSorted = False
+        
+    def findMedian(self) -> float: # log(nlogn)
+        if not self.dataStream:
+            return None
+        if not self.isSorted:
+            self.dataStream.sort()
+            self.isSorted = True
+        size = len(self.dataStream)
+        if size % 2 == 0: # even
+            self.median = (self.dataStream[size // 2 - 1] + self.dataStream[size // 2]) / 2
+        else: # odd
+            self.median = self.dataStream[size // 2]
+        return self.median
+'''
 
 
 # Your MedianFinder object will be instantiated and called as such:
