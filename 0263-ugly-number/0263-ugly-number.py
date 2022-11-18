b17af11094d3a65c567ba920c8d549ff211ffe94
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-11-17 23:20:06 -0500

    Time: 67 ms (28.91%), Space: 13.9 MB (60.08%) - LeetHub

--- 0263-ugly-number/0263-ugly-number.py
+++ 0263-ugly-number/0263-ugly-number.py
@@ -1,12 +1,7 @@
 class Solution:
     def isUgly(self, n: int) -> bool:
-        if n == 0:
-            return False
-        while n % 2 == 0 or n % 3 == 0 or n%5 ==0:
-            if n% 2==0:
-                n /= 2
-            elif n% 3 == 0:
-                n/= 3
-            else:
-                n/=5
+        if n == 0: return False
+        factors = [2,3,5]
+        while sum(not n%f for f in factors):
+            n = (n//2) if n % 2 == 0 else (n//3 if n % 3 == 0 else n//5)
         return n == 1
