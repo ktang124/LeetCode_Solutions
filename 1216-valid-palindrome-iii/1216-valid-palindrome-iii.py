c1d0000a72c18d4f697d5a5ea9b78d9ff07e7dc5
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-11-30 01:18:39 -0500

    Time: 8262 ms (5.19%), Space: 562.7 MB (5.20%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -1,21 +1,26 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
         #recurrence relationship
-        @lru_cache(None)
-        def dfs(s, k):
+        dp = {}
+        def dfs(s, k, dp):
             #print(s, k)
+            if (s,k) in dp:
+                return dp[(s,k)]
             if len(s) <= 1:
                 return True
             #case 1, l and r are the same
             if s[0] == s[-1]:
                 inbetween = s[1:len(s)-1]
-                return dfs(inbetween, k)
+                ans = dfs(inbetween, k,dp)
+                dp[(s,k)] = ans
+                return ans
             else:
                 #either remove s[0] or remove s[-1]
                 if k == 0:
                     return False
                 remStart = s[1:]
                 remLast = s[0:len(s)-1]
-                ans = dfs(remStart, k-1) or dfs(remLast,k-1)
+                ans = dfs(remStart, k-1,dp) or dfs(remLast,k-1,dp)
+                dp[(s,k)] = ans
                 return ans
-        return dfs(s,k)
+        return dfs(s,k,dp)
