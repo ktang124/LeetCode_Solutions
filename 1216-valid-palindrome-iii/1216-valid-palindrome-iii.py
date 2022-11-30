f3309a75ddab94d246ebfec1dd328fe950194da1
ktang124 <86532207+ktang124@users.noreply.github.com>
2022-11-30 01:34:16 -0500

    Time: 1294 ms (46.82%), Space: 25.7 MB (53.37%) - LeetHub

--- 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
+++ 1216-valid-palindrome-iii/1216-valid-palindrome-iii.py
@@ -1,26 +1,23 @@
 class Solution:
     def isValidPalindrome(self, s: str, k: int) -> bool:
         #recurrence relationship
-        dp = {}
-        def dfs(s, k, dp):
+        memo = [[float(inf)] * len(s) for _ in range(len(s))]
+        def dfs(i, j):
             #print(s, k)
-            if (s,k) in dp:
-                return dp[(s,k)]
-            if len(s) <= 1:
-                return True
+            if memo[i][j] != float('inf'):
+                return memo[i][j]
+            if i == j:
+                return 0
+            if i == j-1:
+                return 0 if s[i] == s[j] else 1
             #case 1, l and r are the same
-            if s[0] == s[-1]:
-                inbetween = s[1:len(s)-1]
-                ans = dfs(inbetween, k,dp)
-                dp[(s,k)] = ans
+            if s[i] == s[j]:
+                ans = dfs(i+1,j-1)
+                memo[i][j] = ans
                 return ans
             else:
                 #either remove s[0] or remove s[-1]
-                if k == 0:
-                    return False
-                remStart = s[1:]
-                remLast = s[0:len(s)-1]
-                ans = dfs(remStart, k-1,dp) or dfs(remLast,k-1,dp)
-                dp[(s,k)] = ans
+                ans = 1+min(dfs(i+1,j),dfs(i,j-1))
+                memo[i][j] = ans
                 return ans
-        return dfs(s,k,dp)
+        return dfs(0,len(s)-1) <= k
